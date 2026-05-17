1ee575ac29bb71b4f832c252fea3d4eb66d8dff0
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-05-17 15:01:41 +0530

    card section completed

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,3 +1,4 @@
+import { FaLock,FaThumbtack, FaGlobe,FaChartBar,FaMoneyBillWave } from 'react-icons/fa'
 import '../styles/cards.css'
 function Cards(){
     return(
@@ -19,42 +20,69 @@ function Cards(){
                 <div className="cards-content">
                         <h2>My Cards</h2>
                     <div className='card-section'>
-                        <div className="card1"></div>
-                        <div className="card2"></div>
+                        <div className="card1">
+                          <div className="card-left">
+                            <p style={{fontSize:'24px', color:'white'}}> * * * *   * * * *   4892 </p>
+                            <p>Alex Rodrigues</p>
+                            <p>NexaBank Signature Card</p>
+                          </div>
+                          <div className="card-right">
+                            <h3  style={{paddingTop:'10px',fontFamily:'cursive'}}>VISA</h3>
+                            <p style={{paddingBottom:'40px'}}>Exp: 08/28</p>
+                          </div>
+
+                        </div>
+                        <div className="card2">
+                           <div className="card-left">
+                            <p style={{fontSize:'24px', color:'white'}}> * * * *   * * * *   4892 </p>
+                            <p>Alex Rodrigues</p>
+                            <p>NexaBank Debit Card</p>
+                          </div>
+                          <div className="card-right">
+                            <h3  style={{paddingTop:'10px',fontFamily:'cursive'}}>MC</h3>
+                            <p style={{paddingBottom:'40px'}}>Exp: 04/27</p>
+                          </div>
+                        </div>
                     </div>
                     <div className='card-details'>
                       <div>
                         <p>Credit Limit</p>
-                        <h3>200000</h3>
+                        <h3 style={{color:'blue'}}> {"₹"}200000</h3>
                       </div>
                       <div>
                         <p>Available </p>
-                        <h3>140000</h3></div>
+                        <h3 style={{color:'green'}}> {"₹"}140000</h3></div>
                       <div>
                         <p>Outstanding </p>
-                        <h3>60000</h3>
+                        <h3 style={{color:'orange'}}> {"₹"}60000</h3>
                       </div>
                       <div>
                         <p>Due Date</p>
-                        <h3>May 05</h3>
+                        <h3 style={{color:'orangered'}}>May 05</h3>
                       </div>
                     </div>
-                    <h2>Card Controls</h2>
+                    <h2 className='control-title' >Card Controls</h2>
                     <div className='card-control'>
                       <div>
-                        <p>Block Card</p>
+                       <FaLock style={{color:'yellow'}} />
+                        <p>  Block Card</p>
                       </div>
                       <div>
-                        <p>Set Pin</p>
+                        <FaThumbtack style={{color:'red'}} />
+                        <p>  Set Pin</p>
                       </div>
                       <div>
+                        <FaGlobe />
                         <p>International</p>
                       </div>
                       <div>
-                        <p>Spend Limit</p>
+                        <FaChartBar style={{color:'blueviolet'
+                        }} />
+                        <p>  Spend Limit</p>
                       </div>
                       <div>
-                        <p>Cashback</p>
+                        <FaMoneyBillWave style={{color:'green'}} />
+                        <p>  Cashback</p>
                       </div>
                     </div>
                 </div>
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,9 +1,40 @@
+import '../styles/setting.css'
 function Setting(){
     return(
-        <div className="page-content">
-            <h1>Account Settings</h1>
-            <p>This is the settings page where you can manage your account preferences.</p>
-        </div>
+      <div className="setting">
+        <nav className="setting-navbar">
+                {/* Left Side (Empty to balance the center) */}
+                <div className="nav-side"></div>
+        
+                {/* Center Side */}
+                <div className="nav-center">
+                  <h3>Settings</h3>
+                </div>
+        
+                {/* Right Side */}
+                <div className="nav-side right">
+                  <p>6/6</p>
+                </div>
+              </nav>
+                <div className="setting-content">
+                    <h3>Manage your account preferences and security</h3>
+                   <p>PROFILE</p>
+                   <div className="setting-profile">
+                    <div className="setting-profile-item">
+                        <div className="logo">
+                            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="profile-logo" />
+                        </div>
+                        <div className="name">
+                            <h2>Alex Rodrigues</h2>
+                            <p>alex@123gmail.com</p>
+                        </div>
+                        <div className="edit">
+                            <button>Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+              </div>
     );
 }
 export default Setting;
