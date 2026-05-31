06125e80f07c46cda5d75bfb0b70a5d3b2d7579c
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-05-31 14:57:28 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"; // ← add
 import store from "./store/store.js";
 import "./index.css";
@@ -8,10 +7,10 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+  
   </StrictMode>,
 );
